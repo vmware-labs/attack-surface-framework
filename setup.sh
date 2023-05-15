5e081067030196ea872c65e1437a357471bbdbcf
pavsorab <pavan.sorab@gmail.com>
2023-05-15 12:32:17 +0530

    Reverted kubectl changes for setup.sh
    Signed-off-by: pavsorab <pavan.sorab@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Needs to be run in kali linux - not tested on others
 # Install Graylog
-apt update && apt install -y imagemagick python3-venv psmisc psutils nmap curl wget tcpdump docker.io docker-compose python3-pip golang
+apt update && apt install -y imagemagick python3-venv psmisc psutils nmap curl wget tcpdump docker.io docker-compose python3-pip golang ca-certificates apt-transport-https
 git clone https://github.com/projectdiscovery/nuclei-templates.git /home/nuclei-templates
 cp -R /opt/asf/tools/graylog /
 cd /graylog 
@@ -17,6 +17,35 @@ pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser
+
+# Kubernetes config setup
+KUBE_VARS_FILE=/.kube_vars
+
+if [ -f "$KUBE_VARS_FILE" ]
+then
+	# Load varibale KUBE_FLAG from kube_vars
+    	. $KUBE_VARS_FILE
+else
+	# set default as false
+	KUBE_FLAG=FALSE
+	echo 'KUBE_FLAG=FALSE' > $KUBE_VARS_FILE
+fi
+
+echo "Would you like to use kubernetes cluster (y/n)?"
+read kube_enable
+
+if [ $kube_enable = "y" ]
+then
+	curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+	echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+	apt-get update
+	apt-get install -y kubectl
+	sed -i "s/KUBE_FLAG=$KUBE_FLAG/KUBE_FLAG=TRUE/g" $KUBE_VARS_FILE
+elif [ $kube_enable = "n" ]
+then
+	sed -i "s/KUBE_FLAG=$KUBE_FLAG/KUBE_FLAG=FALSE/g" $KUBE_VARS_FILE
+fi
+
 #Newest version of subfinder
 #Other tools
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
